844094e95f4b9534278d7d979af04ac44e43a7cb
atiquereza <atiquereza@yahoo.com>
2015-07-05 23:12:24 +0600

    Ok With Scripts

---
+++ DigitalVaccination/Scripts/app/UserControllers/UserApp.js
@@ -0,0 +1,43 @@
+﻿var UserApp = angular.module('UserApp', [
+    'ngRoute',
+    'UserControllers'
+]);
+
+angular.element(document).ready(function () {
+    var myHtml = document.getElementById("Container");
+    angular.bootstrap(myHtml, ["UserApp"]);
+
+});
+
+UserApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/showuser', {
+        templateUrl: '/PartialViews/User/showUser.html',
+        controller: 'UserShowController'
+    }).
+
+            when('/createUser', {
+                templateUrl: '/PartialViews/User/EditUser.html',
+                controller: 'EditUserController',
+                 message: 'Create User'
+            }).
+            when('/editUser/:id', {
+
+                templateUrl: '/PartialViews/User/EditUser.html',
+                controller: 'EditUserController',
+                message: 'Edit User'
+            }).
+        when('/deleteUser/:id', {
+            templateUrl: '/PartialViews/User/DeleteUser.html',
+            controller: 'DeleteUserController',
+            message: 'Delete User'
+        }).
+        otherwise({
+            // redirectTo: '/ShowUserInv'
+            redirectTo: function () {
+                window.location.href = "/User/Index";
+
+
+            }
+        });
+
+}]);
